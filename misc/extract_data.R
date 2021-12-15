3480930f4b013ae174e8d024251e6c6dd5a616ef
Jonathan Kitt <jonathan.kitt@inrae.fr>
2021-12-15 13:26:06 +0100

    classic era s11 ok

--- misc/extract_data.R
+++ misc/extract_data.R
@@ -520,6 +520,49 @@ classic_s10_writers <- classic_s10 %>%
 
 rm(classic_s10)
 
+# Classic era - season 11 ----
+
+classic_s11 <- classic_tables[[14]]
+
+names(classic_s11) <- c("story_number", "episode_number", "serial_title", "episode_title",
+                        "director", "writer", "first_aired", "production_code",
+                        "uk_viewers", "rating")
+
+classic_s11 <- classic_s11 %>%
+  mutate(era = "classic",
+         season_number = 11,
+         episode_title = gsub('.*"(.*)".*', "\\1", episode_title),
+         type = "episode",
+         first_aired = as.Date(gsub(".*\\((.*)\\).*", "\\1", first_aired)),
+         production_code = as.character(production_code),
+         duration = 25) %>%
+  group_by(story_number) %>%
+  mutate(episode_number = row_number()) %>%
+  ungroup() %>%
+  mutate(missing_episode = 0) %>%
+  select(era, season_number, serial_title, story_number, episode_number,
+         episode_title, missing_episode, type, director, writer, first_aired,
+         production_code, uk_viewers, rating, duration)
+
+classic_s11_episodes <- classic_s11 %>%
+  select(era:type, first_aired:duration)
+
+classic_s11_directors <- classic_s11 %>%
+  select(era:season_number, story_number, episode_number, director) %>%
+  mutate(director = str_replace(director, "\\s*\\([^\\)]+\\)", "")) %>%
+  separate(director, c("director1", "director2"), " and ") %>%
+  pivot_longer(!(era:episode_number), names_to = "director_name", values_drop_na = TRUE) %>%
+  select(era:episode_number, director = value)
+
+classic_s11_writers <- classic_s11 %>%
+  select(era:season_number, story_number, episode_number, writer) %>%
+  mutate(writer = str_replace(writer, "\\s*\\([^\\)]+\\)", "")) %>%
+  separate(writer, c("writer1", "writer2"), " and ") %>%
+  pivot_longer(!(era:episode_number), names_to = "writer_name", values_drop_na = TRUE) %>%
+  select(era:episode_number, writer = value)
+
+rm(classic_s11)
+
 # Season 02 ----
 
 s02 <- tables[[4]]
