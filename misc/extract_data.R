99094b22e9b7c3100f7b54d11f0518adce8636d2
Jonathan Kitt <jonathan.kitt@inrae.fr>
2021-12-15 11:33:31 +0100

    classic era S07 ok

--- misc/extract_data.R
+++ misc/extract_data.R
@@ -304,7 +304,7 @@ names(classic_s06) <- c("story_number", "episode_number", "serial_title", "episo
 
 classic_s06 <- classic_s06 %>%
   mutate(era = "classic",
-         season_number = 5,
+         season_number = 6,
          episode_title = gsub('.*"(.*)".*', "\\1", episode_title),
          type = "episode",
          first_aired = as.Date(gsub(".*\\((.*)\\).*", "\\1", first_aired)),
@@ -343,6 +343,51 @@ classic_s06_writers <- classic_s06 %>%
   select(era:episode_number, writer = value)
 
 rm(classic_s06)
+
+# Classic era - season 07 ----
+
+classic_s07 <- classic_tables[[10]]
+
+names(classic_s07) <- c("story_number", "episode_number", "serial_title", "episode_title",
+                        "director", "writer", "first_aired", "production_code",
+                        "uk_viewers", "rating")
+
+classic_s07 <- classic_s07 %>%
+  mutate(era = "classic",
+         season_number = 7,
+         episode_title = gsub('.*"(.*)".*', "\\1", episode_title),
+         type = "episode",
+         first_aired = as.Date(gsub(".*\\((.*)\\).*", "\\1", first_aired)),
+         production_code = as.character(production_code),
+         duration = 25) %>%
+  group_by(story_number) %>%
+  mutate(episode_number = row_number()) %>%
+  ungroup() %>%
+  mutate(missing_episode = 0) %>%
+  select(era, season_number, serial_title, story_number, episode_number,
+         episode_title, missing_episode, type, director, writer, first_aired,
+         production_code, uk_viewers, rating, duration)
+
+classic_s07_episodes <- classic_s07 %>%
+  select(era:type, first_aired:duration)
+
+classic_s07_directors <- classic_s07 %>%
+  select(era:season_number, story_number, episode_number, director) %>%
+  mutate(director = str_replace(director, "\\s*\\([^\\)]+\\)", "")) %>%
+  separate(director, c("director1", "director2"), " and ") %>%
+  pivot_longer(!(era:episode_number), names_to = "director_name", values_drop_na = TRUE) %>%
+  select(era:episode_number, director = value)
+
+classic_s07_writers <- classic_s07 %>%
+  select(era:season_number, story_number, episode_number, writer) %>%
+  mutate(writer = case_when(story_number == 53 & episode_number != 1 ~ "David Whitaker and Malcolm Hulke and Terrance Dicks",
+                            TRUE ~ writer)) %>%
+  mutate(writer = str_replace(writer, "\\s*\\([^\\)]+\\)", "")) %>%
+  separate(writer, c("writer1", "writer2", "writer3"), " and ") %>%
+  pivot_longer(!(era:episode_number), names_to = "writer_name", values_drop_na = TRUE) %>%
+  select(era:episode_number, writer = value)
+
+rm(classic_s07)
 
 # Season 02 ----
 
