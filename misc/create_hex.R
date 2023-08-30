255ff24137298f0baceee3504cba1e75c3d1eea8
Jonathan Kitt <jonathan.kitt@inrae.fr>
2023-08-30 15:21:35 +0200

    remove create_hex.R code

--- misc/create_hex.R
+++ misc/create_hex.R
@@ -11,17 +11,6 @@ library(tidyverse)
 library(ggpath)
 
 
-x <- openai::create_image("a dumpster fire in the style of salvador dali with some melted clocks")
-create_image("An astronaut riding a horse in a photorealistic style")
-
-usethis::edit_r_environ(scope = "project")
-OPENAI_API_KEY <- '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'
-
-x <- openai::create_image(prompt = "small tardis in a universe of numbers",
-                     openai_api_key = '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')
-
-
-
 image_read(x$data$url)
 image_write(x$data$url, "misc/datardis.png")
 
